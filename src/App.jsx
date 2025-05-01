33e9aec81a325f6ece28b9590a5c95926e6aad4a
Jadekitan <jadekitan23@gmail.com>
2025-05-01 15:38:01 +0100

    Initial Overview Page UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import './App.css'
 import { Route, Routes } from "react-router-dom";
-import Dashboard from './components/Dashboard';
+import Dashboard from './pages/Dashboard';
 
 function App() {
 
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,122 @@
+import {
+  HStack,
+  VStack,
+  Heading,
+  RadioCard,
+  Icon,
+  Box,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import Overview from "./Overview";
+import { TbDashboard } from "react-icons/tb";
+import { GiProgression } from "react-icons/gi";
+import { LiaChalkboardTeacherSolid } from "react-icons/lia";
+import { IoDocumentText } from "react-icons/io5";
+
+const mainMenu = [
+  {
+    value: "dashboard",
+    title: "Dashboard",
+    icon: <TbDashboard />,
+  },
+  {
+    value: "resources",
+    title: "Resources",
+    icon: <IoDocumentText />,
+  },
+  {
+    value: "progress",
+    title: "Progress",
+    icon: <GiProgression />,
+  },
+  {
+    value: "Practice",
+    title: "Practice",
+    icon: <LiaChalkboardTeacherSolid />,
+  },
+];
+
+const Dashboard = () => {
+  const [value, setValue] = useState("dashboard");
+
+  const renderContent = () => {
+    switch (value) {
+      case "dashboard":
+        return <Overview />;
+      default:
+        return <Overview />;
+    }
+  };
+
+  const renderMenuSection = () => (
+    <VStack w="100%" align="flex-start" gap="20px">
+      {mainMenu.map((item) => (
+        <RadioCard.Item
+          key={item.value}
+          w="100%"
+          h="48px"
+          rounded="16px"
+          bg={value === item.value ? "#4F46E5" : "transparent"}
+          borderWidth="0"
+          value={item.value}
+        >
+          <RadioCard.ItemHiddenInput />
+          <RadioCard.ItemControl py="12px" px="16px">
+            <Icon
+              w="20px"
+              h="20px"
+              color={value === item.value ? "white" : "black"}
+            >
+              {item.icon}
+            </Icon>
+            <RadioCard.ItemText
+              color={value === item.value ? "white" : "black"}
+              fontSize="16px"
+              fontWeight="400"
+              ml="2px"
+            >
+              {item.title}
+            </RadioCard.ItemText>
+          </RadioCard.ItemControl>
+        </RadioCard.Item>
+      ))}
+    </VStack>
+  );
+
+  return (
+    <HStack bg="#F6F7F9" w="100%" h="100%" align="flex-start">
+      <VStack
+        bg="white"
+        w="25%"
+        minH="100vh"
+        justify="start"
+        align="center"
+        py="25px"
+        gap="50px"
+      >
+        <Heading color="#003F88" fontSize="20px" fontWeight="800">
+          AI Interview Buddy
+        </Heading>
+        <VStack w="100%" align="flex-start" px="25px">
+          <RadioCard.Root
+            value={value}
+            onValueChange={(e) => setValue(e.value)}
+            w="100%"
+            orientation="horizontal"
+            align="start"
+            justify="start"
+            maxW="lg"
+            defaultValue="dashboard"
+          >
+            {renderMenuSection()}
+          </RadioCard.Root>
+        </VStack>
+      </VStack>
+      <Box w="75%" h="100%" px="20px">
+        {renderContent()}
+      </Box>
+    </HStack>
+  );
+};
+
+export default Dashboard;
